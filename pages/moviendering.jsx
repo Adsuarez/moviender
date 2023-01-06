1339fe69d3a53acc3f0ad1400f698fe750c365cc
Adsuarez <adsuarezq@gmail.com>
2023-01-06 13:15:49 -0500

    Fail to fetching the movies

--- pages/moviendering.jsx
+++ pages/moviendering.jsx
@@ -1,7 +1,13 @@
+import { FetchMovies } from "./services/FetchMovies.jsx";
+import { useEffect } from "react";
+
 export default function MovienderingPage() {
-    return (
-        <div>
-            <h1>Let's Moviendering</h1>
-        </div>
-    );
+  const movies = FetchMovies();
+  console.log(typeof(movies));
+
+  return (
+    <div>
+      <h1>Lets Moviendering</h1>
+    </div>
+  );
 }
--- pages/services/FetchMovies.jsx
+++ pages/services/FetchMovies.jsx
@@ -1,13 +1,26 @@
 const API_KEY = "3d74f79a7a72cdfde4ba40b495bdde84";
+const API = "https://api.themoviedb.org/3/movie/";
 
-export default async function FetchMovies() {
-  try {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`
-    );
-  } catch (error) {
-    console.log(error);
-  }
+const fetching = () => {
+  return fetch(`${API}550?api_key=${API_KEY}`, {
+    next: { revalidate: 60 },
+  }).then((response) => response.json());
+};
 
-  return <p>{console.log("me llamaron para aqui")}</p>;
+export async function FetchMovies() {
+  const movies = await fetching();
+  const { title } = movies;
+  console.log(movies.title);
+  
+  return title;
 }
+
+/*
+$.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+  }
+});
+*/
