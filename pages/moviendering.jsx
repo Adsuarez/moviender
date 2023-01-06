b69be5fdec81569686d4ef6a47261e4c19ad745c
Adsuarez <adsuarezq@gmail.com>
2023-01-06 16:21:48 -0500

    more errors with FecthMovies

--- pages/moviendering.jsx
+++ pages/moviendering.jsx
@@ -2,12 +2,28 @@ import { FetchMovies } from "./services/FetchMovies.jsx";
 import { useEffect } from "react";
 
 export default function MovienderingPage() {
-  const movies = FetchMovies();
-  console.log(typeof(movies));
+  /*
+  const movies = {
+    then(OnResolve, OnReject) {
+      OnResolve({
+        then(onResolve, onReject) {
+          onResolve(FetchMovies());
+        },
+      });
+    },
+  };
+
+  console.log(Promise.resolve(movies));
+
+  */
+
+  //const movies = FetchMovies;
+  //console.log(movies);
 
   return (
     <div>
       <h1>Lets Moviendering</h1>
+      <FetchMovies />
     </div>
   );
 }
--- pages/services/FetchMovies.jsx
+++ pages/services/FetchMovies.jsx
@@ -4,14 +4,16 @@ const API = "https://api.themoviedb.org/3/movie/";
 const fetching = () => {
   return fetch(`${API}550?api_key=${API_KEY}`, {
     next: { revalidate: 60 },
-  }).then((response) => response.json());
+  })
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
 };
 
 export async function FetchMovies() {
-  const movies = await fetching();
+  const movies = new Promise(fetching());
   const { title } = movies;
   console.log(movies.title);
-  
+
   return title;
 }
 
