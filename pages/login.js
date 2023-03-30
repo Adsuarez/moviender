db4a8f376d27f0c2d5c2157fa8bfd7a182098838
Adsuarez <adsuarezq@gmail.com>
2023-03-30 17:25:17 -0500

    update title and header for more explicity

--- pages/login.js
+++ pages/login.js
@@ -2,20 +2,20 @@
 import Head from "next/head";
 
 //components
-import Login from "components/Login";
+import Auth from "components/Auth";
 
 export default function LoginPage() {
   return (
     <>
       <Head>
-        <title>Login</title>
+        <title>Authorization</title>
         <meta name="Login" content="Form to login" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Login in Moviender</h1>
-      <Login />
+      <h1>Authorization in Moviender</h1>
+      <Auth />
     </>
   );
 }
